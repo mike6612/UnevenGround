97c43d528e90018f7d6f78c008110fa9057e71af
Jason <jason3631061@gmail.com>
2026-02-27 20:05:27 -0600

    Add HighSchool Scene, import graduation cap and elevator, implement elevator opening animation and load next scene feature

---
+++ UnevenGround/Assets/Scripts/HighSchoolScripts/LoadNextScene.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class LoadNextScene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (!collision.gameObject.CompareTag("Player")) { return; }
+        int currentSceneIndex = UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex;
+
+        if (UnityEngine.SceneManagement.SceneManager.sceneCountInBuildSettings <= currentSceneIndex + 1)
+        {
+            Debug.LogWarning("No more scenes to load.");
+            return;
+        }
+
+        UnityEngine.SceneManagement.SceneManager.LoadScene(++currentSceneIndex);
+    }
+}
---
+++ UnevenGround/Assets/Scripts/HighSchoolScripts/TriggerElevatorAnimation.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class TriggerElevatorAnimation : MonoBehaviour
+{
+    Animator animator;
+    // Cache the hash of the bounce state.
+    int m_BounceStateHash;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        m_BounceStateHash = Animator.StringToHash("Base Layer.OpenDoor");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (!collision.gameObject.CompareTag("Player")) { return; }
+
+        if (!animator.GetCurrentAnimatorStateInfo(0).IsName("OpenDoor"))
+        {
+            animator.Play(m_BounceStateHash, 0, 0f);
+        }
+
+    }
+
+}
