fb61b10e7891b1db120ef49664c638c84265222f
Jason <jason3631061@gmail.com>
2026-03-02 21:30:33 -0600

    Implement StreetLightsTrigger script, add StreetLight Box prefab

--- UnevenGround/Assets/Scripts/DarkVoid/TriggerLampLighting.cs
+++ UnevenGround/Assets/Scripts/DarkVoid/TriggerLampLighting.cs
@@ -1,18 +1,23 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class TriggerLampLighting : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] GameObject[] lightsToTurnOn;
+    [SerializeField] GameObject[] lightsToTurnOff;
 
-    // Update is called once per frame
-    void Update()
+    void OnCollisionEnter(Collision collision)
     {
-        
+        if (!collision.gameObject.CompareTag("Player")) { return; }
+
+        foreach (var light in lightsToTurnOn)
+        {
+            light.GetComponentInChildren<Light>().enabled = true;
+        }
+
+        foreach (var light in lightsToTurnOff)
+        {
+            light.GetComponentInChildren<Light>().enabled = false;
+        }
+
     }
 }
