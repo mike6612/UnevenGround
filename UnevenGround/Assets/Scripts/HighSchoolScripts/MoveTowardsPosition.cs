f4623313df73fa755b3df99174f6b6e7e2753931
Jason <jason3631061@gmail.com>
2026-03-10 19:28:53 -0500

    Import graduation typography, implement MoveTowardsPosition for the graduation typography

---
+++ UnevenGround/Assets/Scripts/HighSchoolScripts/MoveTowardsPosition.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class MoveTowardsPosition : MonoBehaviour
+{
+    bool shouldMoveTowardsPosition = false;
+    [SerializeField] float moveSpeed = 1.0f;
+    [SerializeField] Vector3 targetPosition = Vector3.zero;
+    // Start is called before the first frame update
+    void Start()
+    {
+        shouldMoveTowardsPosition = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (shouldMoveTowardsPosition)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, targetPosition, moveSpeed * Time.deltaTime);
+        }
+
+        if (transform.position == targetPosition)
+        {
+            shouldMoveTowardsPosition = false;
+        }
+
+    }
+}
