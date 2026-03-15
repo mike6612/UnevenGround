8eb49ded47dfead56f3a3f1f663886c491bd94a6
Jai Batra <jaibatra@mac.lan>
2026-03-15 14:49:56 -0500

    Added movement to typography

---
+++ UnevenGround/Assets/Scripts/DarkVoid/IntroSentencePulse.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class IntroSentencePulse : MonoBehaviour
+{
+    Vector3 startScale;
+    public float pulseSpeed = 2f;
+    public float pulseAmount = 0.08f;
+
+    void Start()
+    {
+        startScale = transform.localScale;
+    }
+
+    void Update()
+    {
+        float scale = 1 + Mathf.Sin(Time.time * pulseSpeed) * pulseAmount;
+        transform.localScale = startScale * scale;
+    }
+}
