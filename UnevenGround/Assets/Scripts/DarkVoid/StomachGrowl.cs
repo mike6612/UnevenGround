ea91e396a2e2be9f2bf25a4d2ce61e47c8a86b2d
Jai Batra <jaibatra@mac.lan>
2026-03-28 16:34:23 -0500

    Added stomach growl sound

---
+++ UnevenGround/Assets/Scripts/DarkVoid/StomachGrowl.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class StomachGrowl : MonoBehaviour
+{
+    public AudioClip stomachGrowl;
+
+    private AudioSource audioSource;
+    private bool played = false;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (played) return;
+
+        if (other.CompareTag("Player"))
+        {
+            audioSource.PlayOneShot(stomachGrowl);
+            played = true;
+        }
+    }
+}
