3cccb9a75e42c9080b97410f1493c84744bf5432
Jai Batra <jaibatra@mac.lan>
2026-04-02 12:31:55 -0500

    Added Wind Ambient

---
+++ UnevenGround/Assets/Scripts/DarkVoid/CityAmbienceStop.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleCityAudio : MonoBehaviour
+{
+    public AudioSource cityAudio;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (cityAudio.isPlaying)
+            {
+                cityAudio.Stop();
+            }
+            else
+            {
+                cityAudio.Play();
+            }
+        }
+    }
+}
---
+++ UnevenGround/Assets/Scripts/DarkVoid/WindAudio.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindAudio : MonoBehaviour
+{
+    public AudioSource windAudio;
+    private bool canTrigger = true;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") && canTrigger)
+        {
+            canTrigger = false;
+
+            if (windAudio.isPlaying)
+                windAudio.Stop();
+            else
+                windAudio.Play();
+
+            Invoke(nameof(ResetTrigger), 0.5f);
+        }
+    }
+
+    void ResetTrigger()
+    {
+        canTrigger = true;
+    }
+}
