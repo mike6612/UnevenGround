f15410ab3f7004eb5a83ad4aa78db5f81b8bd5ee
Jason <jason3631061@gmail.com>
2026-04-06 18:58:19 -0500

    Activate project description after user steps out of elevator

---
+++ UnevenGround/Assets/Scripts/DarkVoid/ActivateObjectTrigger.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class ActivateObjectTrigger : MonoBehaviour
+{
+    [SerializeField] GameObject objectToActivate;
+    // Start is called before the first frame update
+    void Start()
+    {
+        objectToActivate.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            objectToActivate.SetActive(true);
+        }
+    }
+}
