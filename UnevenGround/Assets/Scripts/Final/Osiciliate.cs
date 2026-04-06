a36009fb6a312112d38985ee3dc6b34d87844f9b
mike6612 <michaeljaimes66@gmail.com>
2026-04-06 01:24:56 -0500

    Finished final scene

--- UnevenGround/Assets/Scripts/Final/Osiciliate.cs
+++ UnevenGround/Assets/Scripts/Final/Osiciliate.cs
@@ -2,12 +2,19 @@
 
 public class Osiciliate : MonoBehaviour
 {
-    //[SerializeField] Vector3 endPosition;
     [SerializeField] float moveSpeed = 1f;
     [SerializeField] float distanceToMove = 1f;
+
     Vector3 startPosition;
     Vector3 endPosition;
+
     float lowerUpperValue = 0f;
+
+    bool isOscillating = true;
+    bool isMovingUp = false;
+
+    float t = 0f;
+
     void Start()
     {
         startPosition = transform.position;
@@ -16,12 +23,41 @@ public class Osiciliate : MonoBehaviour
 
     void Update()
     {
-        ProcessOsiciliation();
+        if (isOscillating)
+        {
+            ProcessOscillation();
+        }
+        else if (isMovingUp)
+        {
+            MoveUpAndDisappear();
+        }
     }
 
-    void ProcessOsiciliation()
+    void ProcessOscillation()
     {
         lowerUpperValue = Mathf.PingPong(Time.time * moveSpeed, 1);
         transform.position = Vector3.Lerp(startPosition, endPosition, lowerUpperValue);
     }
+
+    void MoveUpAndDisappear()
+    {
+        t += Time.deltaTime * moveSpeed;
+
+        float smoothT = Mathf.SmoothStep(0f, 1f, t);
+        transform.position = Vector3.Lerp(transform.position, endPosition + Vector3.up * 10f, smoothT);
+
+        if (t >= 1f)
+        {
+            isMovingUp = false;
+            gameObject.SetActive(false); 
+        }
+    }
+
+    public void TriggerExit()
+    {
+        isOscillating = false;
+        isMovingUp = true;
+        t = 0f;
+    }
+
 }
